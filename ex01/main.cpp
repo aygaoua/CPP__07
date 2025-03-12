b5a978d0e2202fd7828d4993f7121d244371b3aa
aygaoua <zgaoua72@gmail.com>
2025-03-12 02:43:31 +0000

    set my main.cpp in the ex 01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,66 +1,35 @@
 #include <iostream>
 #include "iter.hpp"
 
-// void printInt(int &i) {
-//     std::cout << i << " ";
-// }
-
-// void incrementInt(int &i) {
-//     i++;
-// }
-
-// void printString(std::string &s) {
-//     std::cout << s << " ";
-// }
-
-// int main() {
-//     int intArray[] = {1, 2, 3, 4, 5};
-//     std::string strArray[] = {"Hello", "World", "42"};
-
-//     std::cout << "Original intArray: ";
-//     iter(intArray, 5, printInt);
-//     std::cout << std::endl;
-
-//     std::cout << "Incremented intArray: ";
-//     iter(intArray, 5, incrementInt);
-//     iter(intArray, 5, printInt);
-//     std::cout << std::endl;
-
-//     std::cout << "Original strArray: ";
-//     iter(strArray, 3, printString);
-//     std::cout << std::endl;
-
-//     return 0;
-// }
-
-class Awesome
-{
-  public:
-    Awesome( void ) : _n( 42 ) { return; }
-    int get( void ) const { return this->_n; }
-  private:
-    int _n;
-};
-
-std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
-{
-  o << rhs.get();
-  return o;
+void printInt(int &i) {
+    std::cout << i << " ";
 }
 
-template< typename T >
-void print( T& x )
-{
-  std::cout << x << std::endl;
-  return;
+void incrementInt(int &i) {
+    i++;
+}
+
+void printString(std::string &s) {
+    std::cout << s << " ";
 }
 
 int main() {
-  int tab[] = { 0, 1, 2, 3, 4 };
-  Awesome tab2[5];
+    int intArray[] = {1, 2, 3, 4, 5};
+    std::string strArray[] = {"Hello", "World", "42"};
 
-  iter( tab, 5, print<const int> );
-  iter( tab2, 5, print<Awesome> );
+    std::cout << "Original intArray: ";
+    iter(intArray, 5, printInt);
+    std::cout << std::endl;
 
-  return 0;
+    std::cout << "Incremented intArray: ";
+    iter(intArray, 5, incrementInt);
+    iter(intArray, 5, printInt);
+    std::cout << std::endl;
+
+    std::cout << "Original strArray: ";
+    iter(strArray, 3, printString);
+    std::cout << std::endl;
+
+    return 0;
 }
+
